7facb0be5b31bfbd6e30f6dcaf0fe0ea8f04c02c
minghl <a879307253@gmail.com>
2023-06-26 20:27:46 +0200

    02 MUI 第二版

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import Stack from '@mui/material/Stack';
+// import Button from '@mui/material/Button';
+import {Stack, Button, Typography,Paper} from '@mui/material';
+import { createTheme,ThemeProvider } from '@mui/material/styles';
+import {orange} from '@mui/material/colors';
 
 function App() {
+
+  const theme = createTheme({
+    status: {
+      danger: orange[500],
+    },
+  });
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+    <Paper elevation={3} >
+      <Typography variant="h1" component="h2">
+  h1. Heading
+</Typography>
+<Typography>
+normal text
+</Typography>
+          <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button sx={{color: orange[100]}} size='large' color='error' variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+    </Paper>
+    </ThemeProvider>
+  ); 
 }
 
 export default App;
